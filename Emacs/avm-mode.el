51a4541029675a91c44055392209f9824f09615c
Robert Bocchino <bocchino@me.com>
2013-03-20 15:42:18 -0400

    Fix constant highlighting in AVM mode

--- Emacs/avm-mode.el
+++ Emacs/avm-mode.el
@@ -16,8 +16,10 @@
      (1 'font-lock-keyword-face))
     ("^[A-Za-z0-9\\.]+[[:space:]]+\\([A-Za-z0-9\\.]+\\)" . 
      (1 'font-lock-keyword-face))
+    ("^[A-Za-z0-9\\.]*[\t ]+\\.HS[[:space:]]+\\([[:xdigit:]]+\\)" .
+     (1 'font-lock-constant-face))
     ("\\$[[:xdigit:]]*" . 'font-lock-constant-face)
-    ("[^A-Za-z\\.]\\([[:digit:]]+\\)" .
+    ("[^A-Za-z0-9\\.]\\([[:digit:]]+\\)" .
      (1 'font-lock-constant-face))
     ("'." . 'font-lock-constant-face))
 
